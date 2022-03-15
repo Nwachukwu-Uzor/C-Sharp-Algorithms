a8d764f8f46fa1975bbedea1b65aa4d9bc4ae543
Nwachukwu_Uzor <uzor1997@gmail.com>
2022-03-15 20:43:31 +0100

    submitted the friend or foe algorithm

---
+++ C-Sharp.Core/FriendOFoe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.Core
+{
+    public class FriendOFoe
+    {
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            return names.Where(name => name.Length == 4);
+        }
+    }
+}
---
+++ C-Sharp.Test/FriendOrFoeTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using C_Sharp.Core;
+
+namespace C_Sharp.Test
+{
+    [TestFixture]
+    public class FriendOrFoeTest
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] expected = { "Ryan", "Mark" };
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            CollectionAssert.AreEqual(expected, FriendOFoe.FriendOrFoe(names));
+        }
+    }
+}
