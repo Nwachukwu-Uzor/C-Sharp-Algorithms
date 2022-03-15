e9c253a26963dd71cbfbf2262b06e0addd948d40
Nwachukwu_Uzor <uzor1997@gmail.com>
2022-03-15 21:23:54 +0100

    solving the number of people in bus algorithm

---
+++ C-Sharp.Core/Bus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.Core
+{
+    public class Bus
+    {
+        public static int Number(List<int[]> peopleListInOut)
+        {
+            int total = 0;
+           foreach(var item in peopleListInOut)
+            {
+                total += item[0] - item[1];
+            }
+
+            return total;
+        }
+    }
+}
---
+++ C-Sharp.Test/BusTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using C_Sharp.Core;
+using System.Collections.Generic;
+
+namespace C_Sharp.Test
+{
+    [TestFixture]
+    public class BusTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            List<int[]> peopleList = new List<int[]>() { new[] { 10, 0 }, new[] { 3, 5 }, new[] { 5, 8 } };
+            Assert.AreEqual(5, Bus.Number(peopleList));
+        }
+        [Test]
+        public void SecondTest()
+        {
+            List<int[]> peopleList = new List<int[]>() { new[] { 3, 0 }, new[] { 9, 1 }, new[] { 4, 10 }, new[] { 12, 2 }, new[] { 6, 1 }, new[] { 7, 10 } };
+            Assert.AreEqual(17, Bus.Number(peopleList));
+        }
+        [Test]
+        public void ThirdTest()
+        {
+            List<int[]> peopleList = new List<int[]>() { new[] { 3, 0 }, new[] { 9, 1 }, new[] { 4, 8 }, new[] { 12, 2 }, new[] { 6, 1 }, new[] { 7, 8 } };
+            Assert.AreEqual(21, Bus.Number(peopleList));
+        }
+    }
+}
