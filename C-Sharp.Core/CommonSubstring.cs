57dc115c55deeaee5c9ef174ebcc45358165d3dd
Nwachukwu_Uzor <uzor1997@gmail.com>
2022-03-15 20:12:28 +0100

    first commit

---
+++ C-Sharp.Core/CommonSubstring.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace C_Sharp.Core
+{
+    public class CommonSubstring
+	{
+		public static bool SubstringTest(string str1, string str2)
+		{
+			var value1 = str1.Length >= str2.Length ? str1 : str2;
+			var value2 = value1 == str1 ? str2 : str1;
+
+			var arr1 = GetSubstrings(value1);
+			return CompareSubstrings(arr1, value2);
+		}
+
+		public static List<string> GetSubstrings(string input)
+		{
+			var inputArray = new List<string>();
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				for (int j = 2; j <= input.Length - i; j++)
+				{
+					inputArray.Add(input.Substring(i, j).ToLower());
+				}
+			}
+
+			return inputArray;
+		}
+
+		public static bool CompareSubstrings(List<string> input1, string input2)
+		{
+			foreach (var item in input1)
+			{
+				if (input2.ToLower().Contains(item))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ C-Sharp.Test/CommonSubstringTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using C_Sharp.Core;
+
+namespace C_Sharp.Test
+{
+    [TestFixture]
+    public class SubstringTest
+    {
+        [Test]
+        public void ShouldBeTrue()
+        {
+            Assert.AreEqual(true, CommonSubstring.SubstringTest("Something", "Home"));
+        }
+
+        [Test]
+        public void ShouldBeFalse()
+        {
+            Assert.AreEqual(false, CommonSubstring.SubstringTest("Something", "Fun"));
+        }
+    }
+}
