1defc5a33895d49c4a1c2ce25a1d8e2c8908ffce
Nwachukwu_Uzor <uzor1997@gmail.com>
2022-03-15 21:08:42 +0100

    solved the order please codewars algorithm problem

---
+++ C-Sharp.Core/OrderPlease.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp.Core
+{
+    public class OrderPlease
+    {
+        public static string Order(string words)
+        {
+            var wordsArray = words.Split(' ');
+            var wordsList = new List<string>();
+
+            for (int i = 0; i < wordsArray.Length; i++)
+            {
+                foreach(var item in wordsArray)
+                {
+                    if(item.Contains((i + 1).ToString()))
+                    {
+                        wordsList.Add($"{item} ");
+                    }
+                }
+            }
+
+            return string.Join("", wordsList).Trim();
+        }
+    }
+}
---
+++ C-Sharp.Test/OrderPleaseTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using C_Sharp.Core;
+
+namespace C_Sharp.Test
+{
+    [TestFixture]
+     public class OrderPleaseTest
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("Thi1s is2 3a T4est", OrderPlease.Order("is2 Thi1s T4est 3a"));
+            Assert.AreEqual("Fo1r the2 g3ood 4of th5e pe6ople", OrderPlease.Order("4of Fo1r pe6ople g3ood th5e the2"));
+            Assert.AreEqual("", OrderPlease.Order(""));
+        }
+    }
+}
